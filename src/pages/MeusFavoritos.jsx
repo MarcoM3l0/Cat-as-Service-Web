d85d72d3ebff8435f75051a5717cd88e5b7453da
MarcoM3l0 <josemarcomelonascimento@gmail.com>
2023-04-03 00:33:45 -0300

    feat: opção de remoção de raças da lista.

--- src/pages/MeusFavoritos.jsx
+++ src/pages/MeusFavoritos.jsx
@@ -13,6 +13,7 @@ const urlFavourites = import.meta.env.VITE_FAVOURITES
 const MeusFavoritos = () => {
 
     const [buttoanExcluirDisabled, setButtaonExcluirDisabled] = useState(true)
+    const [loading, setLoading] = useState(true);
     
     //responsável por obter a lista de gatos favoritados
     // Obetem os gatos favoritados 
@@ -30,7 +31,7 @@ const MeusFavoritos = () => {
           };
           
           axios.request(options).then(function (response) {
-            const data = response.data.map(({ image_id }) => ({ image_id }))
+            const data = response.data.map(({ id, image_id }) => ({ id, image_id }))
             setBreadsFavourites(data);
           }).catch(function (error) {
             console.error(error);
@@ -51,18 +52,23 @@ const MeusFavoritos = () => {
         });
     }, [])
 
-    const names = [];
+    const [names, setNames] = useState([])
 
-    breeds.forEach(breed => {
-        breedsFavourites.forEach(favourites => {
-            if(favourites.image_id == breed.id){
-                names.push({
-                    id: breed.id,
-                    name: breed.name
-                })
-            }
-        })
-    })
+    useEffect(() => {
+        const newNames = [];
+        breedsFavourites.forEach(favourite => {
+          const breed = breeds.find(breed => breed.id === favourite.image_id);
+          if (breed) {
+            newNames.push({
+              id: favourite.id,
+              image_id: breed.id,
+              name: breed.name
+            });
+          }
+        });
+        setNames(newNames);
+        setLoading(false)
+      }, [breeds, breedsFavourites]);
     
     /*================================================================================= */
 
@@ -77,43 +83,96 @@ const MeusFavoritos = () => {
       } else {
         // caso contrário, seleciona o item
         setSelectedId(id);
+        console.log(id)
         setButtaonExcluirDisabled(false)
       }
     }
+    /*=================================================================================== */
+
+    // responsável por deletar uma raça dos favoritos
+    const handleSubmitDelete = () =>{
+
+        const options = {
+            method: 'DELETE',
+            url: `${urlFavourites}/${selectedId}`,
+            headers: {
+              'x-api-key': apikey,
+              'Content-Type': 'application/json'
+            }
+        };
+          
+        axios.request(options).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+        const newNames = names.filter(name => name.id !== selectedId);
+        setNames(newNames);
+
+    }
+
     
     return (
         <div className="Container">
-            {names.length === 0 ? <Carregando/> :
-                <div>
-                    <h2 className="titulo">Meus Favoritos</h2>
-                    <p className="Descricao-page">Aqui você encontra a lista de gatinhos favoritos</p>
-                    <hr />
+            {loading ? <Carregando/> :
+                names.length != 0 ?
+                    <div>
+                        <h2 className="titulo">Meus Favoritos</h2>
+                        <p className="Descricao-page">Aqui você encontra a lista de gatinhos favoritos</p>
+                        <hr />
 
-                    <div className="container-favoritos">
-                        <div className="lista-favoritos">
-                            <ul>
-                                {names.map(name =>(
-                                    <li
-                                    className={selectedId === name.id ? 'selected' : 'selectLi'}
-                                    key={name.id} 
-                                    value={name.id}
-                                    onClick={() => handleNameClick(name.id)}
-                                  >
-                                    {name.name}
-                                  </li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className="botoes-Favoritos">
-                            <Link className="btn-Voltar" to="/Cat-as-Service-Web">Voltar</Link>
-                            <button 
-                                className={`btn-Excluir-Favoritar ${buttoanExcluirDisabled ? 'btn-disabled' : ''}`}
-                                disabled={buttoanExcluirDisabled}>
-                                    Excluir Favorito
-                            </button>
+                        <div className="container-favoritos">
+                            <div className="lista-favoritos">
+                                <ul>
+                                    {names.map(name =>(
+                                        <li
+                                        className={selectedId === name.id ? 'selected' : 'selectLi'}
+                                        key={name.image_id} 
+                                        value={name.image_id}
+                                        onClick={() => handleNameClick(name.id)}
+                                    >
+                                        {name.name}
+                                    </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="botoes-Favoritos">
+                                <Link className="btn-Voltar" to="/Cat-as-Service-Web">Voltar</Link>
+                                <button 
+                                    className={`btn-Excluir-Favoritar ${buttoanExcluirDisabled ? 'btn-disabled' : ''}`}
+                                    disabled={buttoanExcluirDisabled}
+                                    onClick={handleSubmitDelete}>
+                                        Excluir Favorito
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    :
+                    <div>
+                        <h2 className="titulo">Meus Favoritos</h2>
+                        <p className="Descricao-page">Sua lista de favoritos está vazia.</p>
+                        <hr />
+                        <div className="container-favoritos">
+                            <div className="lista-favoritos">
+                                <ul>
+                                    <li>Sua lista de favoritos está vazia!</li>
+                                    <li>Procure alguns gatinhos fofinhos</li>
+                                    <li>e adicione ele aqui.</li>
+                                    <li><img src="../img/empty-list-cat.png" height='200px' /></li>
+                                </ul>
+                            </div>
+                            <div className="botoes-Favoritos">
+                                <Link className="btn-Voltar" to="/Cat-as-Service-Web">Voltar</Link>
+                                <button 
+                                    className={`btn-Excluir-Favoritar ${true ? 'btn-disabled' : ''}`}
+                                    disabled={true}>
+                                        Excluir Favorito
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    
             }
         </div>
     )
